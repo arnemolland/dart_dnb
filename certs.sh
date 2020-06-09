27c3aec4e8be787aa7bfdb1505f67d21478a1c0b
Arne Molland <me@arnemolland.com>
2020-06-09 15:57:47 +0200

    Update decode script

--- certs.sh
+++ certs.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 mkdir .certificates &&
 	touch .certificates/private.key &&
 	touch .certificates/certificate.pem &&
-	echo $PSD2_PRIVATE_KEY | base64 --decode >.certificates/private.key &&
-	echo $PSD2_CERTIFICATE | base64 --decode >.certificates/certificate.pem
+	echo $PSD2_PRIVATE_KEY | base64 -d >.certificates/private.key &&
+	echo $PSD2_CERTIFICATE | base64 -d >.certificates/certificate.pem
