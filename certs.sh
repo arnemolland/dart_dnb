31a3e9058ed987b0de054e0e4d3f7160e93579d9
Arne Molland <me@arnemolland.com>
2020-06-10 11:45:39 +0200

    Switch to openssl base64 implementation

--- certs.sh
+++ certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir .certificates &&
+mkdir -p .certificates &&
 	touch .certificates/private.key &&
 	touch .certificates/certificate.pem &&
-	echo $PSD2_PRIVATE_KEY | base64 -d >.certificates/private.key &&
-	echo $PSD2_CERTIFICATE | base64 -d >.certificates/certificate.pem
+	echo -n "$PSD2_PRIVATE_KEY" | openssl base64 -d >.certificates/private.key &&
+	echo -n "$PSD2_CERTIFICATE" | openssl base64 -d >.certificates/certificate.pem
